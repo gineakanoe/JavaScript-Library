f16a6b2bd24f17726f88dfe035d666c3aba29174
Ginea Merrill <gineakanoe@gmail.com>
2021-09-26 14:01:00 -0400

    added info for 3-fetch folder in starwars and unsplash.  Unsplash html is fav so far.

--- 3-API-Fundamentals/3-Fetch/01-Star-Wars/star-wars-scripts.js
+++ 3-API-Fundamentals/3-Fetch/01-Star-Wars/star-wars-scripts.js
@@ -21,19 +21,89 @@
 //* Now can all ".json()" on the response; then follow with another ".then()" after the Promise resolves, so we can use the json we get back!
 
 /*
-let starWarsPeopleList = document.querySelector('ul');
+! let starWarsPeopleList = document.querySelector('ul');
 
-fetch('https://swapi.dev/api/people')
-.then(function(response) {
-    return response.json();
-})
-.then(function(json) {
+! fetch('https://swapi.dev/api/people')
+! .then(function(response) {
+!    return response.json();
+! })
+! .then(function(json) {
 
-});
+! });
 */
 
 
 //*Using the data : can see properties like: "count", "next", "previous", "results"
+//*When console.log, "results" is an array with 10 objects which contain our people info.
+//*We then can capture that infor in a variable.
+
+
+//! let starWarsPeopleList = document.querySelector('ul');
+
+//! fetch('https://swapi.dev/api/people')
+//! .then(function(response) {
+//!     return response.json();
+//! })
+//! .then(function(json) {
+//!     /*console.log(json)*/
+//!     let people = json.results;
+//! });
+
+
+//* GETTING RESULTS TO THE HTML
+    //* our 'people' is an array of objects
+        //*Remember, 'for...of' lets us iterate over values in an array
+        //*Remember, 'for...in' will print out the properties
+        //* console.log both to decide how to handle data
+
+//! let starWarsPeopleList = document.querySelector('ul');
+
+//! fetch('https://swapi.dev/api/people')
+//! .then(function(response) {
+//!     return response.json();
+//! })
+//! .then(function(json) {
+//!    /*console.log(json)*/
+//!    let people = json.results;
+
+//!    for(p of people) {          //Returns object names and details
+//!         console.log(p);
+//!     }
+//!     for(p in people) {          //Returns object number (0-9)
+//!        console.log(p);
+//!    }
+//! });
+        
+
+//*PUTTING TEXT IN OUR LIST ITEMS
+    //*Using ".innerHTML()" we can grab the value of the inner HTML value.
+        //*We're gonna put a '<p>' tag with the name inside in our HTML without manually writing it.
+        //*In our 'for...of' loop, we're refering to each object as 'p' 
+            //*Now we can access the name via 'p.name'.
+
+//! let starWarsPeopleList = document.querySelector('ul');
+
+//! fetch('https://swapi.dev/api/people')
+//! .then(function(response) {
+//!     return response.json();
+//! })
+//! .then(function(json) {
+//!    /*console.log(json)*/
+//!    let people = json.results;
+
+//!    for(p of people) {         
+//!         let listItem = document.createElement('li');
+//!         listItem.innerHTML = '<p>' + p.name + '</p>'
+//!     }
+//! });
+
+
+//*ADDING THE LIST ITEMS TO THE LIST
+    //*We now have our list item with the name inside it
+    //*Now to place all these list Items inside our unordered list:
+        //*We have a variable for 'ul' already called 'starWarsPeopleList'
+        //*Use '.appendChild()' method to add a node to the end of the list of children of a specified parent node.
+
 
 let starWarsPeopleList = document.querySelector('ul');
 
@@ -42,5 +112,43 @@ fetch('https://swapi.dev/api/people')
     return response.json();
 })
 .then(function(json) {
+    /*console.log(json)*/
     let people = json.results;
+
+    for(p of people) {          
+        let listItem = document.createElement('li');
+        listItem.innerHTML = '<p>' + p.name + '</p>'
+        starWarsPeopleList.appendChild(listItem);
+    }
 });
+
+//*REFRESH HTML PAGE
+    //*We should now see some people names from our Star Wars API
+
+//*WHAT WE DID
+/*  1. Made request to Star Wars API
+    2. Received a response from Star Wars API
+    3. Turned that response into something we could use
+    4. Displayed info from Star Wars API in our HTML
+*/
+
+//* WITH PLANETS        
+        //! Just replaced 'people' with 'planets'
+
+let starWarsPlanetsList = document.querySelector('ul');
+
+fetch('https://swapi.dev/api/planets')
+.then(function(response) {
+    return response.json();
+})
+.then(function(json) {
+    let planets = json.results;
+
+    for(p of planets) {          
+        let listItem = document.createElement('li');
+        listItem.innerHTML = '<p>' + p.name + '</p>'
+        starWarsPlanetsList.appendChild(listItem);
+    }
+});
+
+
